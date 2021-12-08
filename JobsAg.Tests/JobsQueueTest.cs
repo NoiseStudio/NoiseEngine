179422922069ad80e3452e6caa3e8b11d30b7659
Vixen <infvix@gmail.com>
2021-12-08 18:44:21 +0100

    Add new test for Jobs Queue

--- JobsAg.Tests/JobsQueueTest.cs
+++ JobsAg.Tests/JobsQueueTest.cs
@@ -14,6 +14,16 @@ namespace NoiseStudio.JobsAg.Tests {
             Assert.Single(world.queue.endQueue);
         }
 
+        [Fact]
+        public void Test2() {
+            JobsWorld world = new JobsWorld(new uint[] { 5, 15 });
+            world.NewJob(Test1, 30);
+
+            Assert.Empty(world.queue.endQueue);
+            Thread.Sleep(30);
+            Assert.Single(world.queue.endQueue);
+        }
+
     }
 }
 
