c1784b8107a06cf94f1e6a0cda70963dfbfb160e
Vixen <infvix@gmail.com>
2021-12-08 15:00:41 +0100

    Document NewJob methods in JobWorld

--- JobsAg/JobWorld.cs
+++ JobsAg/JobWorld.cs
@@ -11,12 +11,26 @@ namespace NoiseStudio.JobsAg {
         public delegate void JobT0();
         public delegate void JobT1<T>(T argument0);
 
+        /// <summary>
+        /// Creates new job in this job world
+        /// </summary>
+        /// <param name="toExecute">The method that will be executed</param>
+        /// <param name="relativeExecutionTime">Relative waiting time in milliseconds to <see cref="Job"/> execution</param>
+        /// <returns><see cref="Job"/></returns>
         public Job NewJob(JobT0 toExecute, ulong relativeExecutionTime) {
             Job job = NewJobWorker(toExecute, relativeExecutionTime);
             AddNewJobToQueue(job);
             return job;
         }
 
+        /// <summary>
+        /// Creates new job in this job world
+        /// </summary>
+        /// <typeparam name="T">First argument type</typeparam>
+        /// <param name="toExecute">The method that will be executed</param>
+        /// <param name="relativeExecutionTime">Relative waiting time in milliseconds to <see cref="Job"/> execution</param>
+        /// <param name="argument0">First argument</param>
+        /// <returns><see cref="Job"/></returns>
         public Job NewJob<T>(JobT1<T> toExecute, ulong relativeExecutionTime, T argument0) {
             Job job = NewJobWorker(toExecute, relativeExecutionTime);
 
