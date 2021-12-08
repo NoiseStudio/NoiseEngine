2c3c2cf851dc0f10803b22dfa3c6321af4943922
Vixen <infvix@gmail.com>
2021-12-08 18:28:37 +0100

    Add Jobs Queue tests

---
+++ JobsAg.Tests/JobsQueueTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using System.Threading;
+
+namespace NoiseStudio.JobsAg.Tests {
+    public class JobsQueueTest {
+
+        [Fact]
+        public void Test1() {
+            JobWorld world = new JobWorld(new uint[] { 5 });
+            world.NewJob(Test1, 10);
+
+            Assert.Empty(world.queue.endQueue);
+            Thread.Sleep(10);
+            Assert.Single(world.queue.endQueue);
+        }
+
+    }
+}
+
