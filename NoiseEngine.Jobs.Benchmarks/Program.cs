5a20e9b33d70066d601af6db200d7cac6863a956
Vixen <infvix@gmail.com>
2021-12-19 11:50:35 +0100

    Add benchmark project

---
+++ NoiseEngine.Jobs.Benchmarks/Program.cs
@@ -0,0 +1,9 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace NoiseEngine.Jobs.Benchmarks {
+    internal class Program {
+        internal static void Main(string[] args) {
+            BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
