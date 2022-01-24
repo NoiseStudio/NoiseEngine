76e4cd1760080bc9883c3093a436b5245e7d7dc1
kkard2 <kkard2.acct@gmail.com>
2022-01-24 18:54:50 +0100

    Add AtomicBool

---
+++ NoiseEngine.Threading.Tests/AtomicBoolTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace NoiseEngine.Threading.Tests {
+    public class AtomicBoolTest {
+
+        [Fact]
+        public void Exchange() {
+            AtomicBool b = new AtomicBool();
+            Assert.False(b.Exchange(true));
+            Assert.True(b.Exchange(true));
+            Assert.True(b.Exchange(false));
+        }
+
+    }
+}
