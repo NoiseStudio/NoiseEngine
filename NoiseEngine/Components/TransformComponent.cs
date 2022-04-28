d7e7fdd1b47b7187f1e2d5b400af1bc06a2dbef8
kkard2 <kkard2.acct@gmail.com>
2022-04-28 11:44:25 +0200

    Edit Transform to TransformComponent, add matrix calculation and constructors with default values

---
+++ NoiseEngine/Components/TransformComponent.cs
@@ -0,0 +1,16 @@
+﻿using NoiseEngine.Jobs;
+using NoiseEngine.Mathematics;
+
+namespace NoiseEngine.Components {
+    public record struct TransformComponent(Float3 Position, Quaternion Rotation, Float3 Scale) : IEntityComponent {
+
+        public TransformComponent(Float3 position, Quaternion rotation) : this(position, rotation, Float3.One) {
+        }
+
+        public TransformComponent(Float3 position) : this(position, Quaternion.Identity) {
+        }
+
+        public Matrix4x4 Matrix => Matrix4x4.Translate(Position) * Matrix4x4.Rotate(Rotation) * Matrix4x4.Scale(Scale);
+
+    }
+}
