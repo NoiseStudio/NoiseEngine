e04a6c681a06cada603bfce4ad81169b1f852f7e
kkard2 <kkard2.acct@gmail.com>
2022-04-29 19:06:54 +0200

    Edit TransformComponent to be readonly, have more efficient IEquatable implementation, add tests

---
+++ NoiseEngine.Tests/Components/TransformComponentTest.cs
@@ -0,0 +1,21 @@
+﻿using AutoFixture.Xunit2;
+using NoiseEngine.Components;
+using Xunit;
+
+namespace NoiseEngine.Tests.Components {
+    public class TransformComponentTest {
+
+        [Theory, AutoData]
+        public void Equals_Self_ReturnsTrue(TransformComponent component) {
+            bool result = component.Equals(component);
+            Assert.True(result);
+        }
+
+        [Theory, AutoData]
+        public void Equals_Different_ReturnsFalse(TransformComponent first, TransformComponent second) {
+            bool result = first.Equals(second);
+            Assert.False(result);
+        }
+
+    }
+}
--- NoiseEngine/Components/TransformComponent.cs
+++ NoiseEngine/Components/TransformComponent.cs
@@ -1,44 +1,45 @@
-﻿using NoiseEngine.Jobs;
+﻿using System.Text;
+using NoiseEngine.Jobs;
 using NoiseEngine.Mathematics;
 
 namespace NoiseEngine.Components {
-    public record struct TransformComponent : IEntityComponent {
+    public readonly record struct TransformComponent : IEntityComponent {
 
-        private Float3 position;
-        private Quaternion rotation;
-        private Float3 scale;
+        private readonly Float3 position;
+        private readonly Quaternion rotation;
+        private readonly Float3 scale;
 
         public Float3 Position {
             get => position;
-            set {
+            init {
                 position = value;
-                CalculateMatrix();
+                Matrix = CalculateMatrix();
             }
         }
 
         public Quaternion Rotation {
             get => rotation;
-            set {
+            init {
                 rotation = value;
-                CalculateMatrix();
+                Matrix = CalculateMatrix();
             }
         }
 
         public Float3 Scale {
             get => scale;
-            set {
+            init {
                 scale = value;
-                CalculateMatrix();
+                Matrix = CalculateMatrix();
             }
         }
 
-        public Matrix4x4 Matrix { get; private set; }
+        public Matrix4x4 Matrix { get; private init; }
 
         public TransformComponent(Float3 position, Quaternion rotation, Float3 scale) {
             this.position = position;
             this.rotation = rotation;
             this.scale = scale;
-            CalculateMatrix();
+            Matrix = Matrix4x4.Translate(position) * Matrix4x4.Rotate(rotation) * Matrix4x4.Scale(scale);
         }
 
         public TransformComponent(Float3 position, Quaternion rotation) : this(position, rotation, Float3.One) {
@@ -47,8 +48,23 @@ namespace NoiseEngine.Components {
         public TransformComponent(Float3 position) : this(position, Quaternion.Identity) {
         }
 
-        private void CalculateMatrix() {
-            Matrix = Matrix4x4.Translate(Position) * Matrix4x4.Rotate(Rotation) * Matrix4x4.Scale(Scale);
+        public bool Equals(TransformComponent other) {
+            return Matrix.Equals(other.Matrix);
+        }
+
+        public override int GetHashCode() {
+            return Matrix.GetHashCode();
+        }
+
+        private Matrix4x4 CalculateMatrix() {
+            return Matrix4x4.Translate(Position) * Matrix4x4.Rotate(Rotation) * Matrix4x4.Scale(Scale);
+        }
+
+        private bool PrintMembers(StringBuilder builder) {
+            builder.Append($"{nameof(Position)} = {Position}, ");
+            builder.Append($"{nameof(Rotation)} = {Rotation}, ");
+            builder.Append($"{nameof(Scale)} = {Scale}");
+            return true;
         }
 
     }
