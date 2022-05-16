5137209188657884dc3090d34c7d026332181558
Vixen <infvix@gmail.com>
2022-05-16 19:40:23 +0200

    Add matrix cache to transform component

---
+++ NoiseEngine.Tests/Components/TransformComponentTest.cs
@@ -0,0 +1,15 @@
+﻿using NoiseEngine.Components;
+using NoiseEngine.Mathematics;
+using Xunit;
+
+namespace NoiseEngine.Tests.Components {
+    public class TransformComponentTest {
+
+        [Fact]
+        public void EqualsTest() {
+            Assert.Equal(new TransformComponent(Float3.Zero), new TransformComponent(Float3.Zero));
+            Assert.NotEqual(new TransformComponent(Float3.Zero), new TransformComponent(Float3.One));
+        }
+
+    }
+}
