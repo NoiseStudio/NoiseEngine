d395475e3b9d1f79756972e86999f72e757a2b38
Vixen <infvix@gmail.com>
2022-05-16 18:45:26 +0200

    Remove IDisposable from JobsInvoker

--- NoiseEngine.Jobs/JobsInvoker.cs
+++ NoiseEngine.Jobs/JobsInvoker.cs
@@ -1,23 +1,10 @@
-﻿using System;
-using System.Collections.Concurrent;
-using System.Threading;
+﻿using NoiseEngine.Common;
+using System;
 
 namespace NoiseEngine.Jobs {
-    public class JobsInvoker : IDisposable {
+    public class JobsInvoker : Destroyable {
 
-        private static readonly object locker = new object();
-
-        private readonly AutoResetEvent toInvokeAutoResetEvent = new AutoResetEvent(false);
-        private readonly ManualResetEvent invokeManualResetEvent = new ManualResetEvent(false);
-        private readonly ConcurrentList<JobsQueue> queues = new ConcurrentList<JobsQueue>();
-        private readonly ConcurrentQueue<(Job, JobsWorld)> toInvoke = new ConcurrentQueue<(Job, JobsWorld)>();
-        private readonly int threadCount;
-
-        private bool works = true;
-        private long waitTime = 0;
-        private int toInvokeAutoResetEventRelease = 0;
-
-        public bool IsDisposed { get; private set; }
+        private readonly JobsInvokerWorker worker;
 
         /// <summary>
         /// Creates new <see cref="JobsInvoker"/>
@@ -25,95 +12,27 @@ namespace NoiseEngine.Jobs {
         /// <param name="threadCount">Number of used threads. When null the number of threads contained in the processor is used.</param>
         /// <exception cref="ArgumentOutOfRangeException">Error when using zero or negative threads</exception>
         public JobsInvoker(int? threadCount = null) {
-            if (threadCount == null)
-                threadCount = Environment.ProcessorCount;
-
-            if (threadCount <= 0)
-                throw new ArgumentOutOfRangeException("The number of threads cannot be zero or negative.");
-
-            this.threadCount = (int)threadCount;
-
-            Thread thread = new Thread(ToInvokeThreadWork);
-            thread.Name = $"{nameof(JobsInvoker)} end queue";
-            thread.Start();
-
-            for (int i = 0; i < threadCount; i++) {
-                thread = new Thread(InvokeThreadWork);
-                thread.Name = $"{nameof(JobsInvoker)} worker #{i}";
-                thread.Start();
-            }
-        }
-
-        ~JobsInvoker() {
-            Abort();
-        }
-
-        /// <summary>
-        /// This <see cref="JobsInvoker"/> will be deactivated and disposed
-        /// </summary>
-        public void Dispose() {
-            lock (this) {
-                if (IsDisposed)
-                    return;
-
-                IsDisposed = true;
-            }
-
-            Abort();
-            GC.SuppressFinalize(this);
+            worker = new JobsInvokerWorker(threadCount);
         }
 
         internal void InvokeJob(Job job, JobsWorld world) {
-            toInvoke.Enqueue((job, world));
-            invokeManualResetEvent.Set();
+            worker.InvokeJob(job, world);
         }
 
         internal void SetToInvokeWaitTime(long waitTime) {
-            if (waitTime < this.waitTime) {
-                Interlocked.Increment(ref toInvokeAutoResetEventRelease);
-                toInvokeAutoResetEvent.Set();
-            }
+            worker?.SetToInvokeWaitTime(waitTime);
         }
 
         internal void AddJobsQueue(JobsQueue queue) {
-            queues.Add(queue);
+            worker.AddJobsQueue(queue);
         }
 
         internal void RemoveJobsQueue(JobsQueue queue) {
-            queues.Remove(queue);
+            worker.RemoveJobsQueue(queue);
         }
 
-        private void ToInvokeThreadWork() {
-            while (works) {
-                Interlocked.Exchange(ref toInvokeAutoResetEventRelease, 0);
-                long newWaitTime = long.MaxValue;
-
-                foreach (JobsQueue queue in queues)
-                    queue.DequeueToInvoke(ref newWaitTime);
-
-                waitTime = newWaitTime;
-                if (waitTime > 0 && toInvokeAutoResetEventRelease != 0) {
-                    toInvokeAutoResetEvent.Reset();
-                    toInvokeAutoResetEvent.WaitOne((int)waitTime);
-                    waitTime = 0;
-                }
-            }
-        }
-
-        private void InvokeThreadWork() {
-            while (works) {
-                invokeManualResetEvent.WaitOne();
-
-                while (toInvoke.TryDequeue(out (Job job, JobsWorld world) jobObject)) {
-                    jobObject.job.Invoke(jobObject.world);
-                }
-
-                invokeManualResetEvent.Reset();
-            }
-        }
-
-        private void Abort() {
-            works = false;
+        protected override void ReleaseResources() {
+            worker.Dispose();
         }
 
     }
