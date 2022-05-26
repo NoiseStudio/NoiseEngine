5c426693d19f7581a0ed2fffa4673eae2cc88b19
Vixen <infvix@gmail.com>
2022-05-26 20:33:33 +0200

    Bump libraries

--- NoiseEngine/DeveloperTools/Systems/DebugMovementSystem.cs
+++ NoiseEngine/DeveloperTools/Systems/DebugMovementSystem.cs
@@ -33,59 +33,58 @@ namespace NoiseEngine.DeveloperTools.Systems {
         protected override void OnUpdateEntity(
             Entity entity, TransformComponent transform, DebugMovementComponent movement
         ) {
-            Float3 position = transform.Position;
-            Quaternion rotation = transform.Rotation;
+            if (!Window.TryGetFocusedWindow(out Window? window))
+                return;
 
+            Input input = Input.GetInput(window);
+
+            Float3 position = transform.Position;
             bool changePosition = false;
 
-            foreach (Window window in application.Windows) {
-                Input input = Input.GetInput(window);
-
-                if (input.ScrollOffset.Y != 0) {
-                    SpeedMultipler = Math.Clamp(
-                        SpeedMultipler * (input.ScrollOffset.Y > 0 ? 1.1f : 0.9f),
-                        MinSpeedMultipler,
-                        MaxSpeedMultipler
-                    );
-                }
-
-                // Move
-                if (input.GetKey(Key.W)) {
-                    position += transform.Front * (speed * SpeedMultipler * DeltaTimeF);
-                    changePosition = true;
-                }
-                if (input.GetKey(Key.S)) {
-                    position += transform.Back * (speed * SpeedMultipler * DeltaTimeF);
-                    changePosition = true;
-                }
-
-                if (input.GetKey(Key.A)) {
-                    position += transform.Left * (speed * SpeedMultipler * DeltaTimeF);
-                    changePosition = true;
-                }
-                if (input.GetKey(Key.D)) {
-                    position += transform.Right * (speed * SpeedMultipler * DeltaTimeF);
-                    changePosition = true;
-                }
-
-                // Rotation
-                Double2 centerPosition = (Double2)window.Size / 2;
-                Double2 deltaPosition = input.CursorPosition - centerPosition;
-
-                movement = movement with {
-                    MouseRotation = new Float2(
-                        Math.Clamp(
-                            (float)deltaPosition.Y * Sensitivity + movement.MouseRotation.X,
-                            -MouseDownLookLimiter,
-                            MouseDownLookLimiter
-                        ),
-                        (float)deltaPosition.X * Sensitivity + movement.MouseRotation.Y
-                    )
-                };
-                rotation = Quaternion.FromEulerAngles(movement.MouseRotation);
-
-                input.CursorPosition = centerPosition;
+            if (input.ScrollOffset.Y != 0) {
+                SpeedMultipler = Math.Clamp(
+                    SpeedMultipler * (input.ScrollOffset.Y > 0 ? 1.1f : 0.9f),
+                    MinSpeedMultipler,
+                    MaxSpeedMultipler
+                );
             }
+
+            // Move
+            if (input.GetKey(Key.W)) {
+                position += transform.Front * (speed * SpeedMultipler * DeltaTimeF);
+                changePosition = true;
+            }
+            if (input.GetKey(Key.S)) {
+                position += transform.Back * (speed * SpeedMultipler * DeltaTimeF);
+                changePosition = true;
+            }
+
+            if (input.GetKey(Key.A)) {
+                position += transform.Left * (speed * SpeedMultipler * DeltaTimeF);
+                changePosition = true;
+            }
+            if (input.GetKey(Key.D)) {
+                position += transform.Right * (speed * SpeedMultipler * DeltaTimeF);
+                changePosition = true;
+            }
+
+            // Rotation
+            Double2 centerPosition = (Double2)(window.Size / 2);
+            Double2 deltaPosition = input.CursorPosition - centerPosition;
+
+            movement = movement with {
+                MouseRotation = new Float2(
+                    Math.Clamp(
+                        (float)deltaPosition.Y * Sensitivity + movement.MouseRotation.X,
+                        -MouseDownLookLimiter,
+                        MouseDownLookLimiter
+                    ),
+                    (float)deltaPosition.X * Sensitivity + movement.MouseRotation.Y
+                )
+            };
+            Quaternion rotation = Quaternion.FromEulerAngles(movement.MouseRotation);
+
+            input.CursorPosition = centerPosition;
 
             if (changePosition) {
                 movement = movement with { TimeUntilLastChangedPosition = 0 };
