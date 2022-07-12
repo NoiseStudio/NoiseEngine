3833a3492605a54538ab2d80b497e02b2a21f2fd
Vixen <infvix@gmail.com>
2022-07-12 21:56:50 +0200

    Document Vector3

--- NoiseEngine/Mathematics/Vector3.cs
+++ NoiseEngine/Mathematics/Vector3.cs
@@ -5,47 +5,103 @@ namespace NoiseEngine.Mathematics;
 
 public readonly record struct Vector3<T>(T X, T Y, T Z) where T : INumber<T> {
 
+    /// <summary>
+    /// Shorthand for writing <see cref="Vector3{T}"/>(T.Zero, T.Zero, T.Zero).
+    /// </summary>
     public static Vector3<T> Zero => new Vector3<T>(T.Zero, T.Zero, T.Zero);
+
+    /// <summary>
+    /// Shorthand for writing <see cref="Vector3{T}"/>(T.One, T.One, T.One).
+    /// </summary>
     public static Vector3<T> One => new Vector3<T>(T.One, T.One, T.One);
 
+    /// <summary>
+    /// Shorthand for writing <see cref="Vector3{T}"/>(-T.One, T.Zero, T.Zero).
+    /// </summary>
     public static Vector3<T> Left => new Vector3<T>(-T.One, T.Zero, T.Zero);
+
+    /// <summary>
+    /// Shorthand for writing <see cref="Vector3{T}"/>(T.One, T.Zero, T.Zero).
+    /// </summary>
     public static Vector3<T> Right => new Vector3<T>(T.One, T.Zero, T.Zero);
+
+    /// <summary>
+    /// Shorthand for writing <see cref="Vector3{T}"/>(T.Zero, T.One, T.Zero).
+    /// </summary>
     public static Vector3<T> Up => new Vector3<T>(T.Zero, T.One, T.Zero);
+
+    /// <summary>
+    /// Shorthand for writing <see cref="Vector3{T}"/>(T.Zero, -T.One, T.Zero).
+    /// </summary>
     public static Vector3<T> Down => new Vector3<T>(T.Zero, -T.One, T.Zero);
+
+    /// <summary>
+    /// Shorthand for writing <see cref="Vector3{T}"/>(T.Zero, T.Zero, T.One).
+    /// </summary>
     public static Vector3<T> Front => new Vector3<T>(T.Zero, T.Zero, T.One);
+
+    /// <summary>
+    /// Shorthand for writing <see cref="Vector3{T}"/>(T.Zero, T.Zero, -T.One).
+    /// </summary>
     public static Vector3<T> Back => new Vector3<T>(T.Zero, T.Zero, -T.One);
 
-    public Vector3(T x) : this(x, T.Zero, T.Zero) {
-    }
-
-    public Vector3(T x, T y) : this(x, y, T.Zero) {
-    }
-
+    /// <summary>
+    /// Calculates squared length of this <see cref="Vector3{T}"/>.
+    /// </summary>
+    /// <returns>Squared length of this <see cref="Vector3{T}"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public T MagnitudeSquared() {
         return X * X + Y * Y + Z * Z;
     }
 
+    /// <summary>
+    /// Calculates dot product of this <see cref="Vector3{T}"/> and <paramref name="rhs"/>.
+    /// </summary>
+    /// <param name="rhs">Second <see cref="Vector3{T}"/>.</param>
+    /// <returns>Dot product of this <see cref="Vector3{T}"/> and <paramref name="rhs"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public T Dot(Vector3<T> rhs) {
         return X * rhs.X + Y * rhs.Y + Z * rhs.Z;
     }
 
+    /// <summary>
+    /// Calculates squared distance of this <see cref="Vector3{T}"/> and <paramref name="rhs"/>.
+    /// </summary>
+    /// <param name="rhs">Second <see cref="Vector3{T}"/>.</param>
+    /// <returns>Squared distance of this <see cref="Vector3{T}"/> and <paramref name="rhs"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public T DistanceSquared(Vector3<T> rhs) {
         return (this - rhs).MagnitudeSquared();
     }
 
+    /// <summary>
+    /// Multiplies this <see cref="Vector3{T}"/> by <paramref name="rhs"/> component-wise.
+    /// </summary>
+    /// <param name="rhs">Second <see cref="Vector3{T}"/>.</param>
+    /// <returns><see cref="Vector3{T}"/> in which every component of this <see cref="Vector3{T}"/>
+    /// is a multiplied by the same component of <paramref name="rhs"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public Vector3<T> Scale(Vector3<T> rhs) {
         return new Vector3<T>(X * rhs.X, Y * rhs.Y, Z * rhs.Z);
     }
 
+    /// <summary>
+    /// Linearly interpolates between <see cref="Vector3{T}"/> and <paramref name="rhs"/>.
+    /// </summary>
+    /// <param name="rhs">End <see cref="Vector3{T}"/>, returned when <paramref name="t"/> = T.One.</param>
+    /// <param name="t">Not clamped value used to interpolate between
+    /// <see cref="Vector3{T}"/> and <paramref name="rhs"/>.</param>
+    /// <returns><see cref="Vector3{T}"/> interpolated value.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public Vector3<T> Lerp(Vector3<T> rhs, T t) {
         return this + (rhs - this) * t;
     }
 
+    /// <summary>
+    /// Calculates cross product of this <see cref="Vector3{T}"/> and <paramref name="rhs"/>.
+    /// </summary>
+    /// <param name="rhs">Second <see cref="Vector3{T}"/>.</param>
+    /// <returns>Cross product of this <see cref="Vector3{T}"/> and <paramref name="rhs"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public Vector3<T> Cross(Vector3<T> rhs) {
         return new Vector3<T>(
@@ -55,34 +111,59 @@ public readonly record struct Vector3<T>(T X, T Y, T Z) where T : INumber<T> {
         );
     }
 
+    /// <summary>
+    /// Adds <paramref name="lhs"/> with <paramref name="rhs"/>.
+    /// </summary>
+    /// <param name="lhs">First <see cref="Vector3{T}"/>.</param>
+    /// <param name="rhs">Second <see cref="Vector3{T}"/>.</param>
+    /// <returns>Result from addition <paramref name="lhs"/> with <paramref name="rhs"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector3<T> operator +(Vector3<T> lhs, Vector3<T> rhs) {
         return new Vector3<T>(lhs.X + rhs.X, lhs.Y + rhs.Y, lhs.Z + rhs.Z);
     }
 
+    /// <summary>
+    /// Subtracts <paramref name="lhs"/> with <paramref name="rhs"/>.
+    /// </summary>
+    /// <param name="lhs">First <see cref="Vector3{T}"/>.</param>
+    /// <param name="rhs">Second <see cref="Vector3{T}"/>.</param>
+    /// <returns>Result from subtraction <paramref name="lhs"/> with <paramref name="rhs"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector3<T> operator -(Vector3<T> lhs, Vector3<T> rhs) {
         return new Vector3<T>(lhs.X - rhs.X, lhs.Y - rhs.Y, lhs.Z - rhs.Z);
     }
 
+    /// <summary>
+    /// Multiplies <paramref name="lhs"/> by <paramref name="rhs"/>.
+    /// </summary>
+    /// <param name="lhs">First <see cref="Vector3{T}"/>.</param>
+    /// <param name="rhs">Second <see cref="Vector3{T}"/>.</param>
+    /// <returns>Result from multiplication <paramref name="lhs"/> by <paramref name="rhs"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector3<T> operator *(Vector3<T> lhs, T rhs) {
         return new Vector3<T>(lhs.X * rhs, lhs.Y * rhs, lhs.Z * rhs);
     }
 
+    /// <summary>
+    /// Divides <paramref name="lhs"/> by <paramref name="rhs"/>.
+    /// </summary>
+    /// <param name="lhs">First <see cref="Vector3{T}"/>.</param>
+    /// <param name="rhs">Second <see cref="Vector3{T}"/>.</param>
+    /// <returns>Result from division <paramref name="lhs"/> by <paramref name="rhs"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector3<T> operator /(Vector3<T> lhs, T rhs) {
         return new Vector3<T>(lhs.X / rhs, lhs.Y / rhs, lhs.Z / rhs);
     }
 
+    /// <summary>
+    /// Return Remainder of <paramref name="lhs"/> divided-by <paramref name="rhs"/>.
+    /// </summary>
+    /// <param name="lhs">First <see cref="Vector3{T}"/>.</param>
+    /// <param name="rhs">Second <see cref="Vector3{T}"/>.</param>
+    /// <returns>The remainder of <paramref name="lhs"/> divided-by <paramref name="rhs"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector3<T> operator %(Vector3<T> lhs, T rhs) {
         return new Vector3<T>(lhs.X % rhs, lhs.Y % rhs, lhs.Z % rhs);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static implicit operator Vector3<T>(T value) {
-        return new Vector3<T>(value);
     }
 
 }
--- NoiseEngine/Mathematics/Vector3Extensions.cs
+++ NoiseEngine/Mathematics/Vector3Extensions.cs
@@ -5,17 +5,33 @@ namespace NoiseEngine.Mathematics;
 
 public static class Vector3Extensions {
 
+    /// <summary>
+    /// Calculates length of <paramref name="vector"/>.
+    /// </summary>
+    /// <param name="vector">This <see cref="Vector3{T}"/>.</param>
+    /// <returns>Length of <paramref name="vector"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static T Magnitude<T>(this Vector3<T> vector) where T : INumber<T>, IRootFunctions<T> {
         return T.Sqrt(vector.MagnitudeSquared());
     }
 
+    /// <summary>
+    /// Normalizes this <paramref name="vector"/>.
+    /// </summary>
+    /// <param name="vector">This <see cref="Vector3{T}"/>.</param>
+    /// <returns>Normalized <paramref name="vector"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector3<T> Normalize<T>(this Vector3<T> vector) where T : INumber<T>, IFloatingPointIeee754<T> {
         T magnitude = vector.Magnitude();
         return magnitude > T.Epsilon ? (vector / magnitude) : Vector3<T>.Zero;
     }
 
+    /// <summary>
+    /// Calculates distance of <paramref name="lhs"/> and <paramref name="rhs"/>.
+    /// </summary>
+    /// <param name="lhs">First <see cref="Vector3{T}"/>.</param>
+    /// <param name="rhs">Second <see cref="Vector3{T}"/>.</param>
+    /// <returns>Distance of <paramref name="lhs"/> and <paramref name="rhs"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static T Distance<T>(this Vector3<T> lhs, Vector3<T> rhs) where T : INumber<T>, IRootFunctions<T> {
         return T.Sqrt(lhs.DistanceSquared(rhs));
