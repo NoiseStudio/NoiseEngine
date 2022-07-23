e96d45295c485564d927d9ab34a47d9c579b64de
Vixen <infvix@gmail.com>
2022-07-23 12:23:50 +0200

    Document Matrix4x4

--- NoiseEngine/Mathematics/Matrix4x4.cs
+++ NoiseEngine/Mathematics/Matrix4x4.cs
@@ -4,23 +4,30 @@ using System.Runtime.InteropServices;
 
 namespace NoiseEngine.Mathematics;
 
+// Matrix layout:
+//
+//     |  C0   C1   C2   C3
+//  ---+---------------------
+//  ---| C0.X C1.X C2.X C3.X
+//  ---| C0.Y C1.Y C2.Y C3.Y
+//  ---| C0.Z C1.Z C2.Z C3.Z
+//  ---| C0.W C1.W C2.W C3.W
+
 [StructLayout(LayoutKind.Sequential)]
 public readonly record struct Matrix4x4<T>(Vector4<T> C0, Vector4<T> C1, Vector4<T> C2, Vector4<T> C3)
     where T : INumber<T>
 {
 
-    // Matrix layout:
-    //
-    //     |  C0   C1   C2   C3
-    //  ---+---------------------
-    //  ---| C0.X C1.X C2.X C3.X
-    //  ---| C0.Y C1.Y C2.Y C3.Y
-    //  ---| C0.Z C1.Z C2.Z C3.Z
-    //  ---| C0.W C1.W C2.W C3.W
-
     public static Matrix4x4<T> Identity =>
         new Matrix4x4<T>(Vector4<T>.Right, Vector4<T>.Up, Vector4<T>.Front, Vector4<T>.Ana);
 
+    /// <summary>
+    /// Creates a scaling <see cref="Matrix4x4{T}"/>.
+    /// </summary>
+    /// <param name="vector"><see cref="Vector3{T}"/> from which scaled
+    /// <see cref="Matrix4x4{T}"/> will be created.</param>
+    /// <returns>Return <see cref="Matrix4x4{T}"/> scaled along
+    /// coordinate axes by a <paramref name="vector"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Matrix4x4<T> Scale(Vector3<T> vector) {
         return new Matrix4x4<T>(
@@ -31,6 +38,12 @@ public readonly record struct Matrix4x4<T>(Vector4<T> C0, Vector4<T> C1, Vector4
         );
     }
 
+    /// <summary>
+    /// Creates a translation <see cref="Matrix4x4{T}"/>.
+    /// </summary>
+    /// <param name="vector"><see cref="Vector3{T}"/> from which translation
+    /// <see cref="Matrix4x4{T}"/> will be created.</param>
+    /// <returns>Return translation <see cref="Matrix4x4{T}"/> from <paramref name="vector"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Matrix4x4<T> Translate(Vector3<T> vector) {
         return new Matrix4x4<T>(
@@ -41,6 +54,12 @@ public readonly record struct Matrix4x4<T>(Vector4<T> C0, Vector4<T> C1, Vector4
         );
     }
 
+    /// <summary>
+    /// Creates a rotation <see cref="Matrix4x4{T}"/>.
+    /// </summary>
+    /// <param name="quaternion"><see cref="Quaternion{T}"/> from which rotation
+    /// <see cref="Matrix4x4{T}"/> will be created.</param>
+    /// <returns>Return rotation <see cref="Matrix4x4{T}"/> from <paramref name="quaternion"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Matrix4x4<T> Rotate(Quaternion<T> quaternion) {
         T x = quaternion.X * NumberHelper<T>.Value2;
@@ -64,6 +83,16 @@ public readonly record struct Matrix4x4<T>(Vector4<T> C0, Vector4<T> C1, Vector4
         );
     }
 
+    /// <summary>
+    /// Creates a orthographic projection <see cref="Matrix4x4{T}"/>.
+    /// </summary>
+    /// <param name="right">The maximum X-value of the view volume.</param>
+    /// <param name="left">The minimum X-value of the view volume.</param>
+    /// <param name="top">The maximum Y-value of the view volume.</param>
+    /// <param name="bottom">The minimum Y-value of the view volume.</param>
+    /// <param name="near">The minimum Z-value of the view volume.</param>
+    /// <param name="far">The maximum Z-value of the view volume.</param>
+    /// <returns>The orthographic projection <see cref="Matrix4x4{T}"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Matrix4x4<T> OrthographicProjection(T right, T left, T top, T bottom, T near, T far) {
         return new Matrix4x4<T>(
@@ -79,6 +108,16 @@ public readonly record struct Matrix4x4<T>(Vector4<T> C0, Vector4<T> C1, Vector4
         );
     }
 
+    /// <summary>
+    /// Creates a perspective projection <see cref="Matrix4x4{T}"/>.
+    /// </summary>
+    /// <param name="right">The maximum X-value of the view volume at the near view plane.</param>
+    /// <param name="left">The minimum X-value of the view volume at the near view plane.</param>
+    /// <param name="top">The maximum Y-value of the view volume at the near view plane.</param>
+    /// <param name="bottom">The minimum Y-value of the view volume at the near view plane.</param>
+    /// <param name="near">The distance to the near view plane.</param>
+    /// <param name="far">The distance to the far view plane.</param>
+    /// <returns>The perspective projection <see cref="Matrix4x4{T}"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Matrix4x4<T> PerspectiveProjection(T right, T left, T top, T bottom, T near, T far) {
         return new Matrix4x4<T>(
@@ -94,17 +133,35 @@ public readonly record struct Matrix4x4<T>(Vector4<T> C0, Vector4<T> C1, Vector4
         );
     }
 
+    /// <summary>
+    /// Returns transformed <paramref name="point"/> by this <see cref="Matrix4x4{T}"/>, with a perspective divide.
+    /// </summary>
+    /// <param name="point"><see cref="Vector3{T}"/> to be used for the transformation.</param>
+    /// <returns>Returns a <paramref name="point"/> transformed by the
+    /// current fully arbitrary <see cref="Matrix4x4{T}"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public Vector3<T> MultiplyPoint(Vector3<T> point) {
         T w = C0.W * point.X + C1.W * point.Y + C2.W * point.Z + C3.W;
         return MultiplyPoint3x4(point) * (T.One / w);
     }
 
+    /// <summary>
+    /// Returns transformed <paramref name="point"/> by this <see cref="Matrix4x4{T}"/>, without a perspective divide.
+    /// </summary>
+    /// <param name="point"><see cref="Vector3{T}"/> to be used for the transformation.</param>
+    /// <returns>Returns a <paramref name="point"/> transformed by the
+    /// current fully arbitrary <see cref="Matrix4x4{T}"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public Vector3<T> MultiplyPoint3x4(Vector3<T> point) {
         return MultiplyVector(point) + new Vector3<T>(C3.X, C3.Y, C3.Z);
     }
 
+    /// <summary>
+    /// Returns transformed <paramref name="vector"/> by this <see cref="Matrix4x4{T}"/>.
+    /// </summary>
+    /// <param name="vector"><see cref="Vector3{T}"/> to be used for the transformation.</param>
+    /// <returns>Returns a <paramref name="vector"/> transformed by the
+    /// current direction of <see cref="Matrix4x4{T}"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public Vector3<T> MultiplyVector(Vector3<T> vector) {
         return new Vector3<T>(
@@ -114,6 +171,13 @@ public readonly record struct Matrix4x4<T>(Vector4<T> C0, Vector4<T> C1, Vector4
         );
     }
 
+    /// <summary>
+    /// Returns the <see cref="Matrix4x4{T}"/> that results from scaling all the elements
+    /// of a specified <see cref="Matrix4x4{T}"/> by a scalar factor.
+    /// </summary>
+    /// <param name="lhs"><see cref="Matrix4x4{T}"/> to scale.</param>
+    /// <param name="vector">Scaling <see cref="Vector4{T}"/> to use.</param>
+    /// <returns>Scaled <see cref="Matrix4x4{T}"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector4<T> operator *(Matrix4x4<T> lhs, Vector4<T> vector) {
         return new Vector4<T>(
@@ -124,6 +188,12 @@ public readonly record struct Matrix4x4<T>(Vector4<T> C0, Vector4<T> C1, Vector4
         );
     }
 
+    /// <summary>
+    /// Returns the <see cref="Matrix4x4{T}"/> that results from multiplying two matrices together.
+    /// </summary>
+    /// <param name="lhs">First <see cref="Matrix4x4{T}"/>.</param>
+    /// <param name="rhs">Second <see cref="Matrix4x4{T}"/>.</param>
+    /// <returns>Product <see cref="Matrix4x4{T}"/>.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Matrix4x4<T> operator *(Matrix4x4<T> lhs, Matrix4x4<T> rhs) {
         return new Matrix4x4<T>(
