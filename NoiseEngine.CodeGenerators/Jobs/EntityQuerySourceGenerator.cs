ad2b1cc26574d66f128e0230e05b0416841eea28
Vixen <infvix@gmail.com>
2022-07-31 18:18:06 +0200

    Add source generators for entity query

---
+++ NoiseEngine.CodeGenerators/Jobs/EntityQuerySourceGenerator.cs
@@ -0,0 +1,188 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.Text;
+
+namespace NoiseEngine.CodeGenerators.Jobs {
+    [Generator]
+    public class EntityQuerySourceGenerator : ISourceGenerator {
+
+        public void Execute(GeneratorExecutionContext context) {
+            for (int i = 0; i <= 8; i++)
+                context.AddSource($"EntityQueryT{i}.generated.cs", SourceText.From(Generate(i), Encoding.UTF8));
+        }
+
+        public void Initialize(GeneratorInitializationContext context) {
+        }
+
+        private string Generate(int tCount) {
+            StringBuilder builder = new StringBuilder(@"// AUTO GENERATED - DO NOT EDIT
+
+#nullable enable
+
+using System.Collections;
+using System.Collections.Concurrent;
+
+namespace NoiseEngine.Jobs;
+
+public class EntityQuery");
+
+            if (tCount > 0) {
+                builder.Append('<');
+
+                for (int i = 1; i <= tCount; i++) {
+                    builder.Append('T');
+                    builder.Append(i);
+
+                    if (i < tCount)
+                        builder.Append(", ");
+                }
+
+                builder.Append('>');
+            }
+
+            builder.Append(" : EntityQueryBase, IEnumerable<");
+
+            AddIEnumeratorArguments(tCount, builder);
+
+            builder.Append('>');
+
+            if (tCount > 0) {
+                builder.AppendLine();
+
+                for (int i = 1; i <= tCount; i++) {
+                    builder.Append("    where T");
+                    builder.Append(i);
+                    builder.AppendLine(" : struct, IEntityComponent");
+                }
+            } else {
+                builder.Append(' ');
+            }
+
+            builder.Append('{');
+            builder.AppendLine();
+
+            if (tCount > 0) {
+                builder.AppendLine();
+
+                for (int i = 1; i <= tCount; i++) {
+                    builder.Append("    internal readonly ConcurrentDictionary<Entity, T");
+                    builder.Append(i);
+                    builder.Append("> components");
+                    builder.Append(i);
+                    builder.Append(';');
+                    builder.AppendLine();
+                }
+            }
+
+            builder.AppendLine(@"
+    public EntityQuery(
+        EntityWorld world, IReadOnlyList<Type>? writableComponents = null, IEntityFilter? filter = null
+    ) : base(world, writableComponents, filter) {");
+
+            for (int i = 1; i <= tCount; i++) {
+                builder.Append("        components");
+                builder.Append(i);
+                builder.Append(" = world.ComponentsStorage.AddStorage<T");
+                builder.Append(i);
+                builder.AppendLine(">();");
+            }
+
+            builder.Append(@"    }
+
+    /// <summary>
+    /// Returns an enumerator that iterates through this <see cref=""EntityQuery""/>.
+    /// </summary>
+    /// <returns>An enumerator that can be used to iterate through this <see cref=""EntityQuery""/>.</returns>
+    public IEnumerator<");
+
+            AddIEnumeratorArguments(tCount, builder);
+
+            builder.AppendLine("> GetEnumerator() {");
+
+            if (tCount > 0) {
+                builder.Append(@"        foreach (Entity entity in Entities) {
+            yield return (entity, ");
+
+                for (int i = 1; i <= tCount; i++) {
+                    builder.Append("components");
+                    builder.Append(i);
+
+                    builder.Append("[entity]");
+
+                    if (i < tCount)
+                        builder.Append(", ");
+                }
+
+                builder.AppendLine(@");
+        }");
+            } else {
+                builder.AppendLine("        return Entities.GetEnumerator();");
+            }
+
+            builder.AppendLine("    }");
+            builder.AppendLine();
+
+            for (int i = 1; i <= tCount; i++) {
+                builder.Append("    internal void SetComponent(Entity entity, T");
+                builder.Append(i);
+                builder.Append(@" component) {
+        ComponentsStorage<Entity>.SetComponent(components");
+                builder.Append(i);
+                builder.AppendLine(@", entity, component);
+    }");
+                builder.AppendLine();
+            }
+
+            if (tCount > 0) {
+                builder.Append(@"    internal override void RegisterGroup(EntityGroup group) {
+        if (");
+
+                for (int i = 1; i <= tCount; i++) {
+                    builder.Append("group.HasComponent(typeof(T");
+                    builder.Append(i);
+                    builder.Append("))");
+
+                    if (i < tCount)
+                        builder.Append(" && ");
+                }
+
+                builder.AppendLine(@")
+            base.RegisterGroup(group);
+    }
+");
+            }
+
+            builder.Append(@"    IEnumerator IEnumerable.GetEnumerator() {
+        return GetEnumerator();
+    }
+
+}
+");
+
+            return builder.ToString();
+        }
+
+        private void AddIEnumeratorArguments(int tCount, StringBuilder builder) {
+            if (tCount == 0) {
+                builder.Append("Entity");
+                return;
+            }
+
+            builder.Append("(Entity entity, ");
+
+            for (int i = 1; i <= tCount; i++) {
+                builder.Append('T');
+                builder.Append(i);
+
+                builder.Append(" component");
+                builder.Append(i);
+
+                if (i < tCount)
+                    builder.Append(", ");
+            }
+
+            builder.Append(")");
+        }
+
+    }
+}
