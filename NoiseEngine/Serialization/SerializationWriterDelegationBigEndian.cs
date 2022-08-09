16e710b0279de6e1c2d3969bc3f78b27ed19684a
Vixen <infvix@gmail.com>
2022-08-09 18:23:40 +0200

    Fix class naming

--- NoiseEngine/Serialization/SerializationWriterDelegationBigEndian.cs
+++ NoiseEngine/Serialization/SerializationWriterDelegationBigEndian.cs
@@ -3,59 +3,59 @@ using System.Buffers.Binary;
 
 namespace NoiseEngine.Serialization;
 
-internal class SerializationWriterDelegationLittleEndian : SerializationWriterDelegation {
+internal class SerializationWriterDelegationBigEndian : SerializationWriterDelegation {
 
     public override void WriteUInt16(ushort obj) {
         Span<byte> span = stackalloc byte[sizeof(ushort)];
-        BinaryPrimitives.WriteUInt16LittleEndian(span, obj);
+        BinaryPrimitives.WriteUInt16BigEndian(span, obj);
         Data.AddRange(span);
     }
 
     public override void WriteUInt32(uint obj) {
         Span<byte> span = stackalloc byte[sizeof(uint)];
-        BinaryPrimitives.WriteUInt32LittleEndian(span, obj);
+        BinaryPrimitives.WriteUInt32BigEndian(span, obj);
         Data.AddRange(span);
     }
 
     public override void WriteUInt64(ulong obj) {
         Span<byte> span = stackalloc byte[sizeof(ulong)];
-        BinaryPrimitives.WriteUInt64LittleEndian(span, obj);
+        BinaryPrimitives.WriteUInt64BigEndian(span, obj);
         Data.AddRange(span);
     }
 
     public override void WriteInt16(short obj) {
         Span<byte> span = stackalloc byte[sizeof(short)];
-        BinaryPrimitives.WriteInt16LittleEndian(span, obj);
+        BinaryPrimitives.WriteInt16BigEndian(span, obj);
         Data.AddRange(span);
     }
 
     public override void WriteInt32(int obj) {
         Span<byte> span = stackalloc byte[sizeof(int)];
-        BinaryPrimitives.WriteInt32LittleEndian(span, obj);
+        BinaryPrimitives.WriteInt32BigEndian(span, obj);
         Data.AddRange(span);
     }
 
     public override void WriteInt64(long obj) {
         Span<byte> span = stackalloc byte[sizeof(long)];
-        BinaryPrimitives.WriteInt64LittleEndian(span, obj);
+        BinaryPrimitives.WriteInt64BigEndian(span, obj);
         Data.AddRange(span);
     }
 
     public override void WriteFloat16(Half obj) {
         Span<byte> span = stackalloc byte[2];
-        BinaryPrimitives.WriteHalfLittleEndian(span, obj);
+        BinaryPrimitives.WriteHalfBigEndian(span, obj);
         Data.AddRange(span);
     }
 
     public override void WriteFloat32(float obj) {
         Span<byte> span = stackalloc byte[sizeof(float)];
-        BinaryPrimitives.WriteSingleLittleEndian(span, obj);
+        BinaryPrimitives.WriteSingleBigEndian(span, obj);
         Data.AddRange(span);
     }
 
     public override void WriteFloat64(double obj) {
         Span<byte> span = stackalloc byte[sizeof(double)];
-        BinaryPrimitives.WriteDoubleLittleEndian(span, obj);
+        BinaryPrimitives.WriteDoubleBigEndian(span, obj);
         Data.AddRange(span);
     }
 
--- NoiseEngine/Serialization/SerializationWriterDelegationLittleEndian.cs
+++ NoiseEngine/Serialization/SerializationWriterDelegationLittleEndian.cs
@@ -3,59 +3,59 @@ using System.Buffers.Binary;
 
 namespace NoiseEngine.Serialization;
 
-internal class SerializationWriterDelegationBigEndian : SerializationWriterDelegation {
+internal class SerializationWriterDelegationLittleEndian : SerializationWriterDelegation {
 
     public override void WriteUInt16(ushort obj) {
         Span<byte> span = stackalloc byte[sizeof(ushort)];
-        BinaryPrimitives.WriteUInt16BigEndian(span, obj);
+        BinaryPrimitives.WriteUInt16LittleEndian(span, obj);
         Data.AddRange(span);
     }
 
     public override void WriteUInt32(uint obj) {
         Span<byte> span = stackalloc byte[sizeof(uint)];
-        BinaryPrimitives.WriteUInt32BigEndian(span, obj);
+        BinaryPrimitives.WriteUInt32LittleEndian(span, obj);
         Data.AddRange(span);
     }
 
     public override void WriteUInt64(ulong obj) {
         Span<byte> span = stackalloc byte[sizeof(ulong)];
-        BinaryPrimitives.WriteUInt64BigEndian(span, obj);
+        BinaryPrimitives.WriteUInt64LittleEndian(span, obj);
         Data.AddRange(span);
     }
 
     public override void WriteInt16(short obj) {
         Span<byte> span = stackalloc byte[sizeof(short)];
-        BinaryPrimitives.WriteInt16BigEndian(span, obj);
+        BinaryPrimitives.WriteInt16LittleEndian(span, obj);
         Data.AddRange(span);
     }
 
     public override void WriteInt32(int obj) {
         Span<byte> span = stackalloc byte[sizeof(int)];
-        BinaryPrimitives.WriteInt32BigEndian(span, obj);
+        BinaryPrimitives.WriteInt32LittleEndian(span, obj);
         Data.AddRange(span);
     }
 
     public override void WriteInt64(long obj) {
         Span<byte> span = stackalloc byte[sizeof(long)];
-        BinaryPrimitives.WriteInt64BigEndian(span, obj);
+        BinaryPrimitives.WriteInt64LittleEndian(span, obj);
         Data.AddRange(span);
     }
 
     public override void WriteFloat16(Half obj) {
         Span<byte> span = stackalloc byte[2];
-        BinaryPrimitives.WriteHalfBigEndian(span, obj);
+        BinaryPrimitives.WriteHalfLittleEndian(span, obj);
         Data.AddRange(span);
     }
 
     public override void WriteFloat32(float obj) {
         Span<byte> span = stackalloc byte[sizeof(float)];
-        BinaryPrimitives.WriteSingleBigEndian(span, obj);
+        BinaryPrimitives.WriteSingleLittleEndian(span, obj);
         Data.AddRange(span);
     }
 
     public override void WriteFloat64(double obj) {
         Span<byte> span = stackalloc byte[sizeof(double)];
-        BinaryPrimitives.WriteDoubleBigEndian(span, obj);
+        BinaryPrimitives.WriteDoubleLittleEndian(span, obj);
         Data.AddRange(span);
     }
 
