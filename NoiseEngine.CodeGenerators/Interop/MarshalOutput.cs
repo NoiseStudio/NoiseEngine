8cec636fa414b82642779c56ee61b40b3327140b
Vixen <infvix@gmail.com>
2022-09-17 14:24:58 +0200

    Edit tuples to structs

---
+++ NoiseEngine.CodeGenerators/Interop/MarshalOutput.cs
@@ -0,0 +1,19 @@
+﻿namespace NoiseEngine.CodeGenerators.Interop;
+
+internal readonly struct MarshalOutput {
+
+    public string UnmarshalledParameterName { get; }
+    public string MarshalledParameterName { get; }
+    public string UnmarshalledType { get; }
+    public string MarshalledType { get; }
+
+    public MarshalOutput(
+        string unmarshalledParameterName, string marshalledParameterName, string unmarshalledType, string marshalledType
+    ) {
+        UnmarshalledParameterName = unmarshalledParameterName;
+        MarshalledParameterName = marshalledParameterName;
+        UnmarshalledType = unmarshalledType;
+        MarshalledType = marshalledType;
+    }
+
+}
