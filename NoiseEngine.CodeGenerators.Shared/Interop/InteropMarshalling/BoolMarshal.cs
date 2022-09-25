3b555f89083be2f2afa4ebaaf06c9a0740952bd1
kkard2 <kkard2.acct@gmail.com>
2022-09-25 17:06:43 +0200

    Add BoolMarshal

---
+++ NoiseEngine.CodeGenerators.Shared/Interop/InteropMarshalling/BoolMarshal.cs
@@ -0,0 +1,19 @@
+﻿namespace NoiseEngine.CodeGenerators.Shared.Interop.InteropMarshalling;
+
+internal class BoolMarshal : InteropMarshal {
+
+    public override string MarshallingType => "bool";
+
+    public override string UnmarshallingType => "NoiseEngine.Interop.InteropBool";
+
+    public override string Marshal(string unmarshalledParameterName, out string marshalledParameterName) {
+        marshalledParameterName = CreateUniqueVariableName();
+        return $"{UnmarshallingType} {marshalledParameterName} = new {UnmarshallingType}({unmarshalledParameterName});";
+    }
+
+    public override string Unmarshal(string marshalledParameterName, out string unmarshalledParameterName) {
+        unmarshalledParameterName = CreateUniqueVariableName();
+        return $"{MarshallingType} {unmarshalledParameterName} = {marshalledParameterName}.Value;";
+    }
+
+}
