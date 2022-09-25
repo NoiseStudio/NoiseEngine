528d71f21b41a2e9831d0ee5df85ce0601f8b7b6
Vixen <infvix@gmail.com>
2022-09-25 22:40:38 +0200

    Add interop option to prelude

--- NoiseEngine.Native/src/interop/prelude.rs
+++ NoiseEngine.Native/src/interop/prelude.rs
@@ -1,4 +1,5 @@
-pub use super::interop_span::InteropSpan;
-pub use super::interop_read_only_span::InteropReadOnlySpan;
 pub use super::interop_array::InteropArray;
+pub use super::interop_option::InteropOption;
+pub use super::interop_read_only_span::InteropReadOnlySpan;
+pub use super::interop_span::InteropSpan;
 pub use super::interop_string::InteropString;
