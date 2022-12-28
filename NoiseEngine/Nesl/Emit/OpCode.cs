665093422436ec0f19ef06d24a5a61284c39303a
Vixen <infvix@gmail.com>
2022-12-28 09:21:18 +0100

    Document op codes in NESIL

--- NoiseEngine/Nesl/Emit/OpCode.cs
+++ NoiseEngine/Nesl/Emit/OpCode.cs
@@ -1,28 +1,101 @@
 ﻿namespace NoiseEngine.Nesl.Emit;
 
+/// <summary>
+/// Op codes of NESIL (NoiseEngine Shader Intermediate Language).
+/// </summary>
+/// <remarks>
+/// Glossary of terms:
+/// <list type="number">
+///     <item>
+///         <term>index of a field/variable</term>
+///         <description>
+///         Valid index of field or variable. Firsts indexes (starts from zero) refers to fields in declaration order.
+///         Next is parameters of current method and later variables in scope, also in declaration order.<br />
+///
+///         Max value of unsigned 32 bit integer (4,294,967,295) is the discarded value.
+///         Support of discarded value must be explicit noted.
+///         </description>
+///     </item>
+///     <item>
+///         <term>supported index type</term>
+///         <description>Currently only unsigned 32 bit integer.</description>
+///     </item>
+/// </list>
+/// </remarks>
 public enum OpCode : ushort {
     #region BranchOperations
 
+    /// <summary>
+    /// Calls method given in second argument with parameters in third argument and
+    /// loads return value to first argument.
+    /// </summary>
+    /// <remarks>
+    /// First argument - a valid index of a field/variable with the type of return type from second argument's method.
+    /// Or discarded value.<br />
+    /// Second argument - a valid assembly method reference.<br />
+    /// Third argument - a valid indexes of a field/variable with the type of next parameters from
+    /// second argument's method. Number of indexes must be same as number of parameters.<br />
+    /// </remarks>
     [OpCodeValidation(typeof(uint), typeof(NeslMethod), typeof(uint[]))]
     Call,
+
+    /// <summary>
+    /// Returns and ends current method. Current method must do not have a return type.
+    /// </summary>
     [OpCodeValidation]
     Return,
+
+    /// <summary>
+    /// Returns and ends current method with returning a value from first argument.
+    /// </summary>
+    /// <remarks>
+    /// First argument - a valid index of a field/variable with the type of return type from method.<br />
+    /// </remarks>
     [OpCodeValidation(typeof(uint))]
     ReturnValue,
 
     #endregion
     #region DefOperations
 
+    /// <summary>
+    /// Defines new variable of first argument type. Index of this variable will be next free index.
+    /// </summary>
+    /// <remarks>
+    /// First argument - a valid assembly type reference.<br />
+    /// </remarks>
     [OpCodeValidation(typeof(NeslType))]
     DefVariable,
 
     #endregion
     #region LoadOperations
 
+    /// <summary>
+    /// Loads to first argument, value from second argument.
+    /// </summary>
+    /// <remarks>
+    /// First argument - a valid index of a field/variable.<br />
+    /// Second argument - a valid index of a field/variable with the type of first argument.<br />
+    /// </remarks>
     [OpCodeValidation(typeof(uint), typeof(uint))]
     Load,
+
+    /// <summary>
+    /// Loads to first argument, constant 32 bit unsigned integer from second argument.
+    /// </summary>
+    /// <remarks>
+    /// First argument - a valid index of a field/variable with the 32 bit unsigned integer type.<br />
+    /// Second argument - constant 32 bit unsigned integer value.<br />
+    /// </remarks>
     [OpCodeValidation(typeof(uint), typeof(uint))]
     LoadUInt32,
+
+    /// <summary>
+    /// Loads to first argument, constant 32 bit float from second argument.
+    /// </summary>
+    /// <remarks>
+    /// First argument - a valid index of a field/variable with the 32 bit float type.<br />
+    /// Second argument - constant 32 bit float value.<br />
+    /// </remarks>
     [OpCodeValidation(typeof(uint), typeof(float))]
     LoadFloat32,
 
