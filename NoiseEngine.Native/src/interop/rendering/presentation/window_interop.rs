c9dc3f746af3a09aa58f84dc20ac584c07e7c24f
Vixen <infvix@gmail.com>
2023-01-10 18:36:21 +0100

    Fix compilation error on Linux

--- NoiseEngine.Native/src/interop/rendering/presentation/window_interop.rs
+++ NoiseEngine.Native/src/interop/rendering/presentation/window_interop.rs
@@ -9,16 +9,15 @@ use crate::{
 extern "C" fn rendering_presentation_window_interop_create(
     title: InteropString, width: u32, height: u32
 ) -> InteropResult<Box<Arc<dyn Window>>> {
-    if cfg!(windows) {
-        #[cfg(target_os = "windows")]
-        crate::rendering::presentation::windows::window::WindowWindows::new(
-            String::from(title).as_str(), width, height
-        )
-    } else {
-        InteropResult::with_err(InvalidOperationError::with_str(
-            "Window is not supported on this device."
-        ).into())
-    }
+    #[cfg(target_os = "windows")]
+    return crate::rendering::presentation::windows::window::WindowWindows::new(
+        String::from(title).as_str(), width, height
+    );
+
+    #[allow(unreachable_code)]
+    InteropResult::with_err(InvalidOperationError::with_str(
+        "Window is not supported on this device."
+    ).into())
 }
 
 #[no_mangle]
