e2530320e86a92e7375138e83558eef365e63a54
Vixen <infvix@gmail.com>
2023-01-14 21:27:50 +0100

    Fix platform universal error

--- NoiseEngine.Native/src/errors/platform/mod.rs
+++ NoiseEngine.Native/src/errors/platform/mod.rs
@@ -1,3 +1,4 @@
+#[cfg(target_os = "windows")]
 pub mod windows;
 
 pub mod platform_universal;
--- NoiseEngine.Native/src/errors/platform/platform_universal.rs
+++ NoiseEngine.Native/src/errors/platform/platform_universal.rs
@@ -6,12 +6,16 @@ use super::windows::win32::Win32Error;
 
 #[derive(Debug)]
 pub enum PlatformUniversalError {
+    None,
+    #[cfg(target_os = "windows")]
     Windows(Win32Error)
 }
 
 impl Error for PlatformUniversalError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         match self {
+            PlatformUniversalError::None => unimplemented!(),
+            #[cfg(target_os = "windows")]
             PlatformUniversalError::Windows(err) => err.source()
         }
     }
@@ -20,11 +24,14 @@ impl Error for PlatformUniversalError {
 impl Display for PlatformUniversalError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", match self {
+            PlatformUniversalError::None => unimplemented!(),
+            #[cfg(target_os = "windows")]
             PlatformUniversalError::Windows(err) => err.to_string()
         })
     }
 }
 
+#[cfg(target_os = "windows")]
 impl From<Win32Error> for PlatformUniversalError {
     fn from(err: Win32Error) -> Self {
         Self::Windows(err)
@@ -34,6 +41,8 @@ impl From<Win32Error> for PlatformUniversalError {
 impl From<PlatformUniversalError> for ResultError {
     fn from(err: PlatformUniversalError) -> Self {
         match err {
+            PlatformUniversalError::None => unimplemented!(),
+            #[cfg(target_os = "windows")]
             PlatformUniversalError::Windows(err) => err.into()
         }
     }
