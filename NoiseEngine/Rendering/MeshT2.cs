682a918e1e1be80c05d395b38ac099ab6eba218a
Vixen <infvix@gmail.com>
2023-01-30 08:27:19 +0100

    Optimize MeshT2 creation

--- NoiseEngine/Rendering/MeshT2.cs
+++ NoiseEngine/Rendering/MeshT2.cs
@@ -1,5 +1,8 @@
 ﻿using NoiseEngine.Rendering.Buffers;
+using NoiseEngine.Rendering.Buffers.CommandBuffers;
 using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Reflection;
 
 namespace NoiseEngine.Rendering;
 
@@ -11,8 +14,36 @@ public class Mesh<TVertex, TIndex> : Mesh where TVertex : unmanaged where TIndex
     public Mesh(
         GraphicsDevice device, ReadOnlySpan<TVertex> vertices, ReadOnlySpan<TIndex> indices
     ) : base(device, GetIndexFormat(typeof(TIndex))) {
-        vertexBuffer = new GraphicsDeviceBuffer<TVertex>(device, GraphicsBufferUsage.Vertex, vertices);
-        indexBuffer = new GraphicsDeviceBuffer<TIndex>(device, GraphicsBufferUsage.Index, indices);
+        ulong verticesCount = (ulong)vertices.Length;
+        ulong indicesCount = (ulong)indices.Length;
+
+        vertexBuffer = new GraphicsDeviceBuffer<TVertex>(device, GraphicsBufferUsage.Vertex, verticesCount);
+        indexBuffer = new GraphicsDeviceBuffer<TIndex>(device, GraphicsBufferUsage.Index, indicesCount);
+
+        GraphicsHostBuffer<TVertex> vertexHost = Device.BufferPool.GetOrCreateHost<TVertex>(
+            GraphicsBufferUsage.TransferAll, verticesCount
+        );
+        vertexHost.SetData(vertices);
+
+        GraphicsHostBuffer<TIndex> indexHost = Device.BufferPool.GetOrCreateHost<TIndex>(
+            GraphicsBufferUsage.TransferAll, indicesCount
+        );
+        indexHost.SetData(indices);
+
+        GraphicsCommandBuffer commandBuffer = new GraphicsCommandBuffer(Device, false);
+
+        commandBuffer.CopyUnchecked(vertexHost, vertexBuffer, stackalloc BufferCopyRegion[] {
+            new BufferCopyRegion(0, 0, GraphicsReadOnlyBuffer<TVertex>.GetSize(verticesCount))
+        });
+        commandBuffer.CopyUnchecked(indexHost, indexBuffer, stackalloc BufferCopyRegion[] {
+            new BufferCopyRegion(0, 0, GraphicsReadOnlyBuffer<TIndex>.GetSize(indicesCount))
+        });
+
+        commandBuffer.Execute();
+        commandBuffer.Clear();
+
+        Device.BufferPool.UnsafeReturnHostToPool(vertexHost);
+        Device.BufferPool.UnsafeReturnHostToPool(indexHost);
     }
 
     internal override (GraphicsReadOnlyBuffer vertexBuffer, GraphicsReadOnlyBuffer indexBuffer) GetBuffers() {
