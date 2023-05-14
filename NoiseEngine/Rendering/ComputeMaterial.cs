507704789c4b40bb0c358e378ae034bda770a4b8
Vixen <infvix@gmail.com>
2023-05-14 17:02:33 +0200

    Add GetKernel method to compute material

--- NoiseEngine/Rendering/ComputeMaterial.cs
+++ NoiseEngine/Rendering/ComputeMaterial.cs
@@ -1,4 +1,6 @@
-﻿namespace NoiseEngine.Rendering;
+﻿using NoiseEngine.Nesl;
+
+namespace NoiseEngine.Rendering;
 
 public class ComputeMaterial : CommonMaterial {
 
@@ -6,6 +8,18 @@ public class ComputeMaterial : CommonMaterial {
 
     public ComputeMaterial(ComputeShader shader) : base(shader) {
         Shader = shader;
+    }
+
+    /// <summary>
+    /// Tries return <see cref="ComputeKernel"/> created from given <paramref name="neslMethod"/>.
+    /// </summary>
+    /// <param name="neslMethod">Origin <see cref="NeslMethod"/> of returned <see cref="ComputeKernel"/>.</param>
+    /// <returns>
+    /// <see cref="ComputeKernel"/> when this <see cref="ComputeShader"/> contains given
+    /// <paramref name="neslMethod"/>; otherwise null.
+    /// </returns>
+    public ComputeKernel? GetKernel(NeslMethod neslMethod) {
+        return Shader.GetKernel(neslMethod);
     }
 
 }
