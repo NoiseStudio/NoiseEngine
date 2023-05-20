0f5fa0de6fca2c0302339f1a180aa5c52bd4a3c0
Vixen <infvix@gmail.com>
2023-05-20 17:45:49 +0200

    Fix remove creating current type for file when not in use

--- NoiseEngine.Tests/Nesl/CompilerTools/Parsing/Structs/Universal.cs
+++ NoiseEngine.Tests/Nesl/CompilerTools/Parsing/Structs/Universal.cs
@@ -1,9 +1,20 @@
 ﻿using NoiseEngine.Nesl;
 using NoiseEngine.Tests.Environments;
+using System.Linq;
 
 namespace NoiseEngine.Tests.Nesl.CompilerTools.Parsing.Structs;
 
 public class Universal : NeslParsingTestEnvironment {
+
+    [Fact]
+    public void Define() {
+        NeslAssembly assembly = CompileSingle(@"
+            struct HelloWorld {}
+        ");
+
+        Assert.Single(assembly.Types);
+        Assert.Equal("HelloWorld", assembly.Types.Single().Name);
+    }
 
     [Fact]
     public void DefineExisting() {
