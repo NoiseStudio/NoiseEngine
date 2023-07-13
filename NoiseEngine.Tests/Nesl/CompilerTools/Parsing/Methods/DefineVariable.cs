db858bf414b298797474e45bf85c6728cf0ffb59
Vixen <infvix@gmail.com>
2023-07-13 14:14:37 +0200

    Add define variables in NESL method

---
+++ NoiseEngine.Tests/Nesl/CompilerTools/Parsing/Methods/DefineVariable.cs
@@ -0,0 +1,51 @@
+﻿using NoiseEngine.Nesl;
+using NoiseEngine.Tests.Environments;
+
+namespace NoiseEngine.Tests.Nesl.CompilerTools.Parsing.Methods;
+
+public class DefineVariable : NeslParsingTestEnvironment {
+
+    [Fact]
+    public void DefineWithoutAssignment() {
+        CompileSingle(@"
+            f32 Main(f32 a) {
+                f32 temp;
+                temp = a;
+                return temp;
+            }
+        ");
+    }
+
+    [Fact]
+    public void DefineWithAssignment() {
+        CompileSingle(@"
+            f32v3 Main(f32v3 a) {
+                f32v3 temp = a;
+                return temp;
+            }
+        ");
+    }
+
+    [Fact]
+    public void DefineWithoutAssignmentMultipleWithSameName() {
+        CompileSingleThrow(@"
+            f32v3 Main(f32v3 a) {
+                f32v3 temp = a;
+                f32v3 temp = a;
+                return temp;
+            }
+        ", CompilationErrorType.VariableAlreadyExists);
+    }
+
+    [Fact]
+    public void DefineWithAssignmentMultipleWithSameName() {
+        CompileSingleThrow(@"
+            f32v3 Main(f32v3 a) {
+                f32v3 temp = a;
+                f32v3 temp = a;
+                return temp;
+            }
+        ", CompilationErrorType.VariableAlreadyExists);
+    }
+
+}
---
+++ NoiseEngine/Nesl/CompilerTools/Parsing/Expressions/VariableDeclaration.cs
@@ -0,0 +1,24 @@
+﻿using NoiseEngine.Nesl.CompilerTools.Parsing.Tokens;
+
+namespace NoiseEngine.Nesl.CompilerTools.Parsing.Expressions;
+
+internal class VariableDeclaration : ParserExpressionContainer {
+
+    public VariableDeclaration(Parser parser) : base(parser) {
+    }
+
+    [ParserExpression(ParserStep.Method)]
+    [ParserExpressionParameter(ParserTokenType.TypeIdentifier)]
+    [ParserExpressionParameter(ParserTokenType.Name)]
+    [ParserExpressionTokenType(ParserTokenType.Semicolon)]
+    public void Define(TypeIdentifierToken typeIdentifier, NameToken name) {
+        DefineWorker(typeIdentifier, name);
+    }
+
+    public VariableData? DefineWorker(TypeIdentifierToken typeIdentifier, NameToken name) {
+        if (!Parser.TryGetType(typeIdentifier, out NeslType? type))
+            return null;
+        return Parser.DefineVariable(type, name);
+    }
+
+}
