5b548d3dfa2c10b4ebc4070198f52b39c365c150
Vixen <infvix@gmail.com>
2023-07-19 14:46:05 +0200

    Add missing documentation

--- NoiseEngine/Jobs/IThreadStorage.cs
+++ NoiseEngine/Jobs/IThreadStorage.cs
@@ -2,6 +2,13 @@
 
 public interface IThreadStorage<TThreadStorage> where TThreadStorage : IThreadStorage<TThreadStorage> {
 
+    /// <summary>
+    /// Creates a new instance of <typeparamref name="TThreadStorage"/> for the given <paramref name="entitySystem"/>.
+    /// </summary>
+    /// <param name="entitySystem">
+    /// <see cref="EntitySystem{TThreadStorage}"/> which creates this <typeparamref name="TThreadStorage"/> instance.
+    /// </param>
+    /// <returns>New not used by other threads <typeparamref name="TThreadStorage"/> instance.</returns>
     public static abstract TThreadStorage Create(EntitySystem<TThreadStorage> entitySystem);
 
 }
