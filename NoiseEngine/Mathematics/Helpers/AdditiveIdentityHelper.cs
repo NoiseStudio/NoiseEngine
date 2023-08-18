a1a5ae0bd40f275599ffa97094d49b635b374bb3
Vixen <infvix@gmail.com>
2023-08-18 22:00:50 +0200

    Fix formatting

--- NoiseEngine/Mathematics/Helpers/AdditiveIdentityHelper.cs
+++ NoiseEngine/Mathematics/Helpers/AdditiveIdentityHelper.cs
@@ -2,8 +2,7 @@
 
 namespace NoiseEngine.Mathematics.Helpers;
 
-internal static class AdditiveIdentityHelper<TSelf, TResult> where TSelf : IAdditiveIdentity<TSelf, TResult>
-{
+internal static class AdditiveIdentityHelper<TSelf, TResult> where TSelf : IAdditiveIdentity<TSelf, TResult> {
 
     public static TResult AdditiveIdentity => TSelf.AdditiveIdentity;
 
