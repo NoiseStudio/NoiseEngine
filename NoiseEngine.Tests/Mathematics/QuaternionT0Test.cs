11b76bf61377e6c2312119e48b657c361a110b27
Vixen <infvix@gmail.com>
2023-10-02 17:26:41 +0200

    Add quaternion look rotation

--- NoiseEngine.Tests/Mathematics/QuaternionT0Test.cs
+++ NoiseEngine.Tests/Mathematics/QuaternionT0Test.cs
@@ -16,4 +16,23 @@ public class QuaternionT0Test {
         ), a);
     }
 
+    [Theory]
+    [InlineData(
+        new float[] { 0.81f, 0.59f, -0.06f, 0.34f, 0.0f, 0.94f },
+        new float[] { 0.39524522f, 0.61128134f, 0.6399186f, 0.24621218f }
+    )]
+    [InlineData(
+        new float[] { 0.13f, -0.04f, -0.99f },
+        new float[] { 0.0013059094f, 0.9976699f, -0.019975385f, 0.065223604f }
+    )]
+    public void LookRotation(float[] init, float[] expected) {
+        Quaternion<float> a = Quaternion.LookRotation(
+            new float3(init[0], init[1], init[2]),
+            init.Length > 3 ? new float3(init[3], init[4], init[5]) : float3.Up
+        );
+        Assert.Equal(new Quaternion<float>(
+            expected[0], expected[1], expected[2], expected[3]
+        ), a);
+    }
+
 }
--- NoiseEngine/Mathematics/QuaternionT0.cs
+++ NoiseEngine/Mathematics/QuaternionT0.cs
@@ -71,4 +71,79 @@ public static class Quaternion {
         return EulerDegrees(angles.X, angles.Y, angles.Z);
     }
 
+    /// <summary>
+    /// Creates a rotation which rotates <paramref name="forward"/>s to upwards directions.
+    /// Where upwards is always <see cref="Vector3{T}.Up"/>.
+    /// </summary>
+    /// <param name="forward">The forward direction. Direction to orient towards.</param>
+    /// <returns>The calculated <see cref="Quaternion{T}"/>.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Quaternion<T> LookRotation<T>(Vector3<T> forward) where T : IFloatingPointIeee754<T> {
+        return LookRotation(forward, Vector3<T>.Up);
+    }
+
+    /// <summary>
+    /// Creates a rotation which rotates <paramref name="forward"/>s to <paramref name="up"/>wards directions.
+    /// </summary>
+    /// <param name="forward">The forward direction. Direction to orient towards.</param>
+    /// <param name="up">
+    /// Up direction. Contrains y axis orientation to a plane this vector lies on. This rule might be broken if
+    /// <paramref name="forward"/> and <paramref name="up"/> direction are nearly parallel.
+    /// </param>
+    /// <returns>The calculated <see cref="Quaternion{T}"/>.</returns>
+    public static Quaternion<T> LookRotation<T>(Vector3<T> forward, Vector3<T> up) where T : IFloatingPointIeee754<T> {
+        forward = forward.Normalize();
+        Vector3<T> rightNorm = up.Cross(forward).Normalize();
+        Vector3<T> upNorm = forward.Cross(rightNorm);
+
+        T m00 = rightNorm.X;
+        T m01 = rightNorm.Y;
+        T m02 = rightNorm.Z;
+        T m10 = upNorm.X;
+        T m11 = upNorm.Y;
+        T m12 = upNorm.Z;
+        T m20 = forward.X;
+        T m21 = forward.Y;
+        T m22 = forward.Z;
+
+        T sum = m00 + m11 + m22;
+        if (sum > T.Zero) {
+            T num = T.Sqrt(sum + T.One);
+            T inv = NumberHelper<T>.Half / num;
+            return new Quaternion<T>(
+                (m12 - m21) * inv,
+                (m20 - m02) * inv,
+                (m01 - m10) * inv,
+                num * NumberHelper<T>.Half
+            );
+        } else if (m00 >= m11 && m00 >= m22) {
+            T num = T.Sqrt(T.One + m00 - m11 - m22);
+            T inv = NumberHelper<T>.Half / num;
+            return new Quaternion<T>(
+                num * NumberHelper<T>.Half,
+                (m01 + m10) * inv,
+                (m02 + m20) * inv,
+                (m12 - m21) * inv
+            );
+        } else if (m11 > m22) {
+            T num = T.Sqrt(T.One + m11 - m00 - m22);
+            T inv = NumberHelper<T>.Half / num;
+            return new Quaternion<T>(
+                (m10 + m01) * inv,
+                num * NumberHelper<T>.Half,
+                (m21 + m12) * inv,
+                (m20 - m02) * inv
+            );
+        } else {
+            T num = T.Sqrt(T.One + m22 - m00 - m11);
+            T inv = NumberHelper<T>.Half / num;
+            return new Quaternion<T>(
+                (m20 + m02) * inv,
+                (m21 + m12) * inv,
+                num * NumberHelper<T>.Half,
+                (m01 - m10) * inv
+            );
+        }
+    }
+
 }
