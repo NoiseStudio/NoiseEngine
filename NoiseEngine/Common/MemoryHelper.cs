0efc3218e5a640016a78bc877f2a9561797de20c
aaroniziyaev-win <aaroni@post.bgu.ac.il>
2023-11-04 21:55:34 +0200

    reformat & doc

--- NoiseEngine/Common/MemoryHelper.cs
+++ NoiseEngine/Common/MemoryHelper.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Runtime.InteropServices;
 
 namespace NoiseEngine.Common;
 
@@ -7,10 +7,23 @@ namespace NoiseEngine.Common;
 /// </summary>
 internal static class MemoryHelper
 {
-    public static nuint AlignmentOf<T>() where T : unmanaged
-    {
-        return (nuint)System.Runtime.InteropServices.Marshal.SizeOf<T>();
-    }
     
-    public static bool IsDangling<T>(nuint address) where T: unmanaged => address == AlignmentOf<T>();
+    /// <summary>
+    /// Returns the alignment requirement (in bytes) of an unmanaged type.
+    /// </summary>
+    /// <typeparam name="T">The unmanaged type for which to determine the alignment.</typeparam>
+    /// <returns>The alignment requirement (in bytes) of the specified unmanaged type.</returns>
+    public static nuint AlignmentOf<T>() where T : unmanaged {
+        return (nuint)Marshal.SizeOf<T>();
+    }
+
+    /// <summary>
+    /// Checks if the provided memory address is considered dangling for a specified unmanaged type.
+    /// </summary>
+    /// <typeparam name="T">The unmanaged type for which to perform the check.</typeparam>
+    /// <param name="address">The memory address to check.</param>
+    /// <returns>True if the memory address is equal to the alignment requirement of the specified unmanaged type; otherwise, false.</returns>
+    public static bool IsDangling<T>(nuint address) where T : unmanaged {
+        return address == AlignmentOf<T>();    
+    }
 }
