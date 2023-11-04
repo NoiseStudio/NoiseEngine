f677e84441bdd4bb2cc2634c1d75cc988d1a37b9
aaroniziyaev-win <aaroni@post.bgu.ac.il>
2023-11-04 19:08:36 +0200

    Alignment check and dangling check same as rust's impl

---
+++ NoiseEngine/Common/MemoryHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NoiseEngine.Common;
+
+/// <summary>
+/// Collection of utility helpers memory management.
+/// </summary>
+internal static class MemoryHelper
+{
+    public static nuint AlignmentOf<T>() where T : unmanaged
+    {
+        return (nuint)System.Runtime.InteropServices.Marshal.SizeOf<T>();
+    }
+    
+    public static bool IsDangling<T>(nuint address) where T: unmanaged => address == AlignmentOf<T>();
+}
