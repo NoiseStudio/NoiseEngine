0b0cd8e259a9fab7c8f0c5a4be261dd90b3de51b
aaroniziyaev-win <aaroni@post.bgu.ac.il>
2023-11-07 17:12:04 +0200

    Reformat 2

--- NoiseEngine/Interop/InteropArray.cs
+++ NoiseEngine/Interop/InteropArray.cs
@@ -60,8 +60,7 @@ public struct InteropArray<T> : IDisposable, IReadOnlyList<T> where T : unmanage
     /// Creates a new <see cref="InteropArray{T}"/> with the specified <paramref name="length"/>.
     /// </summary>
     /// <param name="length">Length of the array.</param>
-    public unsafe InteropArray(int length)
-    {
+    public unsafe InteropArray(int length) {
         var size = MemoryHelper.AlignmentOf<T>();
         pointer = (T*)NativeMemory.AlignedAlloc(size * (nuint)length, size);
         Length = length;
